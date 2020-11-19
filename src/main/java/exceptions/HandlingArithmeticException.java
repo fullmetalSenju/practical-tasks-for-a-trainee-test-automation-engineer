d6a8a127e631924f78fa8388903101128e492c6f
Nata <Mazhara>
2020-11-19 03:26:47 +0200

    hometask 10

---
+++ src/main/java/exceptions/HandlingArithmeticException.java
@@ -0,0 +1,24 @@
+package exceptions;
+
+/**
+ * Task:
+ * Handle a runtime exception to finish the program correctly.
+ * Use the "try-catch" block.
+ *
+ * Задача:
+ * Обработка исключения времени выполнения для правильного завершения программы.
+ * Используйте блок "try-catch".
+ */
+public class HandlingArithmeticException {
+    public static void main(String[] args) {
+        try {
+            final int dividend = 6;
+            final int divisor = 0;
+            final int quotient = dividend / divisor;
+            System.out.println("Quotient of " + dividend + " / " + divisor + " = " + quotient);
+
+        } catch(ArithmeticException e) {
+            System.out.println("Cannot be divided by 0");
+        }
+     }
+}
