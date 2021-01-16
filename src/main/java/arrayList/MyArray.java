d4df0dad72418a03eadaf2c6b214a84cff9d8c21
Nata <natalinaschneider@gmail.com>
2021-01-16 15:26:37 +0200

    homework16 0.1

--- src/main/java/arrayList/MyArray.java
+++ src/main/java/arrayList/MyArray.java
@@ -1,4 +1,21 @@
 package arrayList;
 
+import java.util.Arrays;
+
 public class MyArray {
+
+    public static void main(String[] args) {
+        
+        Double a[] = new Double[10];
+        Double b[] = new Double[1];
+        
+        for ( int i = 0; i <= a.length; i++ ){
+            if(i < a.length){
+                a[i] = Math.random();
+            }else{
+                b = Arrays.copyOf(a, (int)(a.length * 1.5));
+            }
+        }
+        System.out.println(Arrays.toString(b));
+    }
 }
